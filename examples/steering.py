8794ccae2c3ab49511e1b878de9a1cf82697fc97
alexoelkers <alexoelkers@gmail.com>
2024-12-02 15:10:56 +0100

    functional multi goal steering

--- examples/steering.py
+++ examples/steering.py
@@ -6,11 +6,9 @@ from urdfenvs.urdf_common.bicycle_model import BicycleModel
 
 MAX_STEER = 0.87270631
 L_ARROW = 0.5
+R_GOAL = 0.1
 
-def run_prius(n_steps=1000, render=False, goal=True, obstacles=True):
-    goal = np.array([-5, 4])
-    fig, ax = plt.subplots()
-    fig2, ax2, = plt.subplots()
+def run_prius(goal_queue, n_steps=1000, render=False, goal=True, obstacles=True):
     robots = [
         BicycleModel(
             urdf='prius.urdf',
@@ -27,38 +25,38 @@ def run_prius(n_steps=1000, render=False, goal=True, obstacles=True):
     env: UrdfEnv = UrdfEnv(
         dt=0.01, robots=robots, render=render
     )
-    action = np.array([1, 0])
+    action = np.array([0, 0])
+    goal = goal_queue.pop(0)
     pos0 = np.array([0, 0, 0])
     ob = env.reset(pos=pos0)
+    ob, *_ = env.step(action)
     print(f"Initial observation : {ob}")
     history = []
-    x = []
-    y = []
-    steering_angle = []
     control_action = []
     for i in range(n_steps):
-        ob, *_ = env.step(action)
+        # get goal 
+        if distance_metric(ob, goal) <= R_GOAL:
+            if len(goal_queue) == 0:
+                print(f"path complete")
+                break
+            goal = goal_queue.pop(0)
+
+        # get action
         action = controller(ob, goal)
-        control_action.append(action[1])
+        control_action.append(action)
+
+        # run action
+        ob, *_ = env.step(action)
         history.append(ob)
-        current_pos = ob['robot_0']['joint_state']['position']
-        x.append(current_pos[0])
-        y.append(current_pos[1])
-        steering_angle.append(ob['robot_0']['joint_state']['steering'][0] + current_pos[2])
-    steering_angle = np.array(steering_angle)
-    ax.plot(x, y)
-    ax.scatter(*goal)
-    ax2.plot(np.arange(0, 10, 0.01), control_action)
-    for i, x_point, y_point, theta in zip(range(len(x)), x, y, steering_angle):
-        if i % 57 == 0:
-            ax.arrow(x_point, y_point, L_ARROW*np.cos(theta), L_ARROW*np.sin(theta), width=0.02)
-    ax.set_aspect("equal")
-    fig.savefig("./examples/circle_car.png")
-    fig2.savefig("./examples/input.png")
+    
     print(f"Final observations: {history[-1]}")
     env.close()
-    return history
+    return history, control_action
 
+def distance_metric(state, goal):
+    """a simple distance metric"""
+    position = state['robot_0']['joint_state']['position']
+    return np.hypot(goal[1] - position[1], goal[0] - position[0]) # euclidian distance
 
 def controller(state, goal):
     """A simple controller to send the current state towards the goal"""
@@ -68,6 +66,37 @@ def controller(state, goal):
     steering_angle = angle_to_goal - position[2]
     return np.array([1, steering_angle])
 
+def create_plots(state_history, input_history, goal_queue):
+    """a function to create plots for a test run"""
+    fig, ax = plt.subplots()
+    fig2, ax2, = plt.subplots()
+
+    time = np.arange(0, 0.01*len(state_history), 0.01)
+    x = []
+    y = []
+    steering_angle = []
+
+    for state, control_action in zip(state_history, input_history):
+        current_pos = state['robot_0']['joint_state']['position']
+        x.append(current_pos[0])
+        y.append(current_pos[1])
+        steering_angle.append(state['robot_0']['joint_state']['steering'][0] + current_pos[2])
+
+    ax.plot(x, y)
+    # ax2.plot(np.arange(0, 100, 0.01), np.array(input_history))
+    for i, x_point, y_point, theta in zip(range(len(x)), x, y, steering_angle):
+        if i % 57 == 0:
+            ax.arrow(x_point, y_point, L_ARROW*np.cos(theta), L_ARROW*np.sin(theta), width=0.02)
+
+    for goal in goal_queue:
+        ax.scatter(goal[0], goal[1])
+    
+    ax.set_aspect("equal")
+    fig.savefig("./examples/circle_car.png")
+    fig2.savefig("./examples/input.png")
+    print(f"plotting succesful")
 
 if __name__ == "__main__":
-    history = run_prius(n_steps=1000, render=False)
+    goal_queue = [(-5, 5), (5, -5), (5, 5), (-5, -5)]
+    history, control_action = run_prius(goal_queue, n_steps=10000, render=False)
+    create_plots(history, control_action, goal_queue)
