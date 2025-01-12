5250ccb2980faa7f9c9dd21fdb8418c64e53144d
Filip Sandström <filip.j.sandstrom@gmail.com>
2025-01-12 15:22:15 +0100

    Decreased velocity delta between tests

--- opengen/test_one_moving_car.py
+++ opengen/test_one_moving_car.py
@@ -14,17 +14,32 @@ from SolverError import SolverError
 def get_obstacle_definition(velocity):
         return [(102.3, 2, 0, velocity)]
 
+def get_nearest_distance(x_history, obstacles_history):
+    collisions = []
+    min_dist = 10000
+    for step, t in enumerate(np.arange(0, T * DT, DT)):
+        for obstacle in range(OBS_N):
+            x_delta = x_history[step, 0] - obstacles_history[obstacle, step, 0]
+            y_delta = x_history[step, 1] - obstacles_history[obstacle, step, 1]
+            distance = np.sqrt(x_delta**2 + y_delta**2)
+
+            if distance < min_dist:
+                min_dist = distance
+                collisions = (obstacle, obstacles_history[obstacle, step, 0], obstacles_history[obstacle, step, 1], distance)
+
+    return collisions
+
 
 def main():
     """a function to test the traffic density which the solver can tollerate"""
-    #compile_solver()
+    compile_solver()
 
     mng = og.tcp.OptimizerTcpManager('my_optimizers/navigation_obstacle', port=12345)
 
     mng.start()
     mng.ping()
 
-    for velocity in np.linspace(-0.5, -5, 25):
+    for velocity in np.linspace(-0, -2.5, 51):
 
         # Initial car state: x, y, theta, velocity (v), steering angle (phi)
         x = np.array([0, 0, 0, 0, 0])
@@ -38,6 +53,7 @@ def main():
             u_history = np.array(u_history)
             obstacle_history = np.array(obstacle_history)
             collisions = collision_detector(x_history, obstacle_history)
+            #print(get_nearest_distance(x_history, obstacle_history)[3])
             if collisions == []:
                  print(f"solver success for velocity = {velocity}")
             else:
